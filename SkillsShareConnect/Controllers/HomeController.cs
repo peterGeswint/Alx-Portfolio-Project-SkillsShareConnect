375055b03a79d01854e9b520c259e9a32bf8e4eb
Peter Clifton Geswint <115699568+peterGeswint@users.noreply.github.com>
2024-09-11 22:15:22 +0200

    Landing

--- SkillsShareConnect/Controllers/HomeController.cs
+++ SkillsShareConnect/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace SkillsShareConnect.Controllers
             return View();
         }
 
+        public IActionResult Generic()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ SkillsShareConnect/Controllers/SkillsShareConnectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SkillsShareConnect.Controllers
+{
+	public class SkillsShareConnectController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
